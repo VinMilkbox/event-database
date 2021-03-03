37f3ac99abb44aa9e68f6b889e3c6d781c76f854
Vincenzo Francavilla <vincenzo.francavilla@milkbox.com>
2021-03-03 15:08:09 +0100

    Implemented in test loop to fetch data from sample file.

--- src/dataManager.js
+++ src/dataManager.js
@@ -11,7 +11,8 @@ const knex = require('knex')({
 });
 exports.dbClient = knex;
 
-const transactionsTable = 'transactions';
+const camelToSnakeCase = str => str.replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`);
+const transactionsTable = 'transactions_events';
 const transactionDetails = 'transaction_details';
 
 exports.printDotEnv = () => {
@@ -21,12 +22,13 @@ exports.parseData = (data) => {
     let parse = JSON.parse(JSON.stringify(data));
     let rebuildObj = [];
     for (const [key, value] of Object.entries(parse)) {
+        let convertKey = camelToSnakeCase(key);
         if(typeof value == "object"){
             for (const [key1, value1] of Object.entries(value)) {
-                rebuildObj[key] = value1;
+                rebuildObj[convertKey] = value1;
             }
         }else{
-            rebuildObj[key] = value;
+            rebuildObj[convertKey] = value;
         }
     }
     return Object.assign({}, rebuildObj);
@@ -38,45 +40,20 @@ exports.parseData = (data) => {
  * @returns {Promise<Knex.QueryBuilder<{transaction_id: string|string|*, amount: string|PaymentCurrencyAmount|*, credit_card_first_6_digits: null, converted_currency: null, created_at: number, type: string, version: string, card_id: string, zip_code: null, client_id: null, credit_card_number_masked: null, auth_amount: null, void_amount: null, domain_name: null, account_id: null, credit_card_last_4_digits: null, province: null, source_reference_customer_id: null, converted_amount: null, refund_amount: null, currency: string|string|*, customer_id: string, status: *, sale_amount: null}, number[]>>}
  */
 exports.createTransaction = (objectStream) => {
-    /// NULL are field without date from objectStream
-    const fieldsRows = {
-        transaction_id: objectStream.transactionId, // This must be implemented on database.
-        type: objectStream.entityType,
-        status: objectStream.status,
-        amount: objectStream.amount,
-        currency: objectStream.currency, // this currency is customer currency?
-        converted_amount: null, // related to currency conversion?
-        converted_currency: null, // reference of the currency converted
-        auth_amount: null, //  may is related to the type field? (auth, sale, capture)?
-        sale_amount: null, //  may is related to the type field? (auth, sale, capture)?
-        void_amount: null, //  may is related to the type field? (auth, sale, capture)?
-        refund_amount: null, //  may is related to the type field? (auth, sale, capture)?
-        created_at: new Date().getTime(),
-        card_id: objectStream.cardId,
-        account_id: null,// missing on sample data
-        domain_name: null,// missing on sample data
-        customer_id: objectStream.customerId,
-        province: null,// missing on sample data
-        zip_code: null,// missing on sample data
-        credit_card_last_4_digits: null, // missing on sample data
-        credit_card_first_6_digits: null, // missing on sample data
-        credit_card_number_masked: null, // missing on sample data
-        version: objectStream.aggregateVersion, // not sure of this
-        source_reference_customer_id: null, // missing on sample data
-        client_id: null, // missing on sample data
-    };
-    return knex(transactionsTable).insert(fieldsRows);
+    return knex(transactionsTable).insert(objectStream);
 }
 
 /**
  *
- * @param objectStream
+ * @param objectStream Object
  * @returns {Promise<Knex.QueryBuilder<TRecord, number>>}
  */
 exports.updateTransaction = async (objectStream) => {
     return knex(transactionsTable)
         .where({
-            transaction_id: objectStream.transactionId
+            transaction_id: objectStream.transaction_id
+        }).andWhere({
+            sub_transaction_id: objectStream.sub_transaction_id,
         })
         .update({
             status: objectStream.status,
--- test/dataManager.test.js
+++ test/dataManager.test.js
@@ -1,65 +1,24 @@
 require('iconv-lite').encodingExists('foo')
 const dataManagerTest = require('../src/dataManager');
-const sampleData = require('../sampleData/single.json');
-const sampleDataObj = {
-    pk: "a#trans#trans_1",
-    sk: "a#trans#trans_1",
-    transactionId: "trans_1",
-    customerId: "cust_1",
-    organisationId: "org_1",
-    amount: "1",
-    currency: "usd",
-    status: "pending",
-    entityType: "Transaction",
-    createdDate: "2021-02-04T20:37:00",
-    refundedDate: "2021-02-05T20:37:00",
-    chargebackDate: "2021-02-06T20:37:00",
-    partition: "a#trans#2",
-    timestamp: "1612485499",
-    gsi1pk: "cust_1",
-    gsi1sk: "a#trans#trans_1",
-    gsi2pk: "a#card#card_1",
-    gsi2sk: "a#trans#trans_1",
-    gsi3pk: "pending",
-    gsi3sk: "2021-02-06T20:37:00",
-    cardId: "a#card#card_1",
-    aggregateVersion: "2",
-};
+const sampleEventsData = require('../sampleData/event.json');
+const sampleCardsData = require('../sampleData/cards-events.json');
 
 describe('Test dataManager', () => {
-    let id;
-
-    /*test('Checking createTransaction', async () => {
-        let result = await dataManagerTest.createTransaction(sampleDataObj);
-        expect(result).toBeTruthy();
-        id = result[0];
-    });
-
-    test('Test single method findTransaction', async () => {
-        let result = await dataManagerTest.findTransaction({
-            id: id
-        });
-        expect(result).toBeTruthy();
-    });
-
-    test('Test single method findTransaction', async () => {
-        let result = await dataManagerTest.updateTransaction({
-            id: id
-        });
-        expect(result).toBeTruthy();
-    });*/
-
     test('Wrapping up all methods', async () => {
-        let dataSample = dataManagerTest.parseData(sampleData);
-        let find = await dataManagerTest.findTransaction({transaction_id: dataSample.transactionId});
-        if(find){
-            dataSample.status = 'Updated';
-            await dataManagerTest.updateTransaction(dataSample);
-            await dataManagerTest.dbClient.destroy();
-        }else {
-            await dataManagerTest.createTransaction(sampleDataObj);
-            await dataManagerTest.dbClient.destroy();
+        let parse = JSON.parse(JSON.stringify(sampleEventsData));
+        for (const [key, value] of Object.entries(parse)) {
+            let dataSample = dataManagerTest.parseData(value);
+            let find = await dataManagerTest.findTransaction({
+                transaction_id: dataSample.transaction_id,
+                sub_transaction_id: dataSample.sub_transaction_id
+            });
+            if(find){
+                await dataManagerTest.updateTransaction(dataSample);
+            }else {
+                await dataManagerTest.createTransaction(dataSample);
+            }
         }
+        await dataManagerTest.dbClient.destroy();
     });
 });
 
